dec9ba81a3acdb6f41f3688f739899de44324c0d
bernardo040 <bayro.urquidi@escola.pr.gov.br>
2025-11-03 09:57:03 -0300

    n

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(avanca)
 
 avanca.forEach(button =>{
 button.addEventListener('click', function (){
-     const atual = document.querySelector('ativo')
+     const atual = document.querySelector('.ativo')
 const proximoPasso = "passo-" + this.getAttribute('data-proximo')
 atual.classList.remove('ativo')
 document. getElementById(proximoPasso).classList.add('ativo')
